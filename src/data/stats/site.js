09631c7cb0d9750bf0c15f7a460cdcf6acf2a97f
Amin Salarkia <amin.salarkia@freiheit.com>
2025-03-15 15:10:21 +0100

    Use modern-resume-theme

--- src/data/stats/site.js
+++
@@ -1,7 +0,0 @@
-/* Keys match keys returned by the github api. Fields without keys are
- * mostly jokes. To see everything returned by the github api, run:
- */
-const data = [
-];
-
-export default data;
